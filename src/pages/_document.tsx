2abb31b98e2cbcdc70b896b9642464a8a240bdba
unknown <foradorif@gmail.com>
2023-10-02 19:49:58 -0300

    feat: google search

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document () {
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
             <link href="https://fonts.googleapis.com/css2?family=Jost:wght@200;300;400&display=swap" rel="stylesheet"></link>
+            <meta name="google-site-verification" content="W0WmtRA-gm0pS7uMe-7rwmipfegkYjBHHYCdd0BuwOk" />
             <link rel="shortcut icon" href="/assets/beer.ico" />
         </Head>
         <body className="bg-app h-screen bg-cover bg-center bg-no-repeat bg-fixed overflow-x-hidden">
