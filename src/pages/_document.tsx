2ce6adac10adbfe31c06ba16ff7830ed76a2d84e
Pedro Foradori <analistamobile.logtech@trouw.com.br>
2025-12-09 21:50:35 -0300

    fix: ajuste de texto e identacao de codigo(rollback)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,17 +2,6 @@ import { Html, Head, Main, NextScript, } from "next/document";
 
 export default function Document() {
 
-    function isMobileDevice() {
-        if (typeof navigator === 'undefined' || typeof window === 'undefined') return false;
-        const ua = navigator.userAgent || navigator.vendor || (window as any).opera;
-        const uaMobile = /Mobi|Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(ua);
-        const hasTouch = 'ontouchstart' in window || (navigator.maxTouchPoints && navigator.maxTouchPoints > 0);
-        const smallScreen = window.innerWidth <= 768;
-
-        console.log(uaMobile || hasTouch || smallScreen)
-        return uaMobile || hasTouch || smallScreen;
-    }
-
     return (
         <Html>
             <Head>
@@ -22,22 +11,18 @@ export default function Document() {
                 <meta name="google-site-verification" content="W0WmtRA-gm0pS7uMe-7rwmipfegkYjBHHYCdd0BuwOk" />
                 <link rel="shortcut icon" href="/assets/beer.ico" />
             </Head>
-
             <body className="bg-app h-screen bg-cover bg-center bg-no-repeat bg-fixed overflow-x-hidden bg-black 
                              max-sm:bg-app-mobile">
-                {!isMobileDevice() && (
-                    <video preload="metadata" autoPlay muted loop className="fixed inset-0 w-full h-full object-cover z-0 pointer-events-none">
-                        <source src="/assets/bg_video.mp4" type="video/mp4" />
-                        <track
-                            src="/path/to/captions.vtt"
-                            kind="subtitles"
-                            srcLang="en"
-                            label="English"
-                        />
-                        Your browser does not support the video tag.
-                    </video>
-                )}
-
+                <video preload="metadata" autoPlay muted loop className="fixed inset-0 w-full h-full object-cover z-0 pointer-events-none">
+                    <source src="/assets/bg_video.mp4" type="video/mp4" />
+                    <track
+                        src="/path/to/captions.vtt"
+                        kind="subtitles"
+                        srcLang="en"
+                        label="English"
+                    />
+                    Your browser does not support the video tag.
+                </video>
                 <div className="relative z-10">
                     <Main />
                     <NextScript />
